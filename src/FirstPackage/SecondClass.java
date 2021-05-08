31d864a4eab6a7afd8d3ddef7ffc3e5b9ba99ef8
MariamShaliniJack31 <mariamshalini@gmail.com>
2021-05-08 19:31:15 +0530

    FirstProject - Initial Commit

---
+++ src/FirstPackage/SecondClass.java
@@ -0,0 +1,14 @@
+package FirstPackage;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=5;
+		int y=5;
+		int z= (x*y);
+		System.out.println("z :" + z);
+	}
+
+}
